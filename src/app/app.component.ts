2829f737a9277dd6e93b8fcaa967f237b2f95618
Daniel Fischer <DFischer85@comcast.net>
2021-08-31 23:26:43 -0400

    Card component finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      userName: 'nature',
+      content: 'I saw this neat tree today.',
+      imageUrl: 'assets/tree.jpeg'
+    },
+    {
+      title: 'Snow Mountain',
+      userName: 'mountainlover',
+      content: 'Here is a picture of a snowy mountain.',
+      imageUrl: 'assets/mountain.jpeg'
+
+    },
+    {
+      title: 'Mountain Biking',
+      userName: 'biking122',
+      content: 'I went mountain biking today.',
+      imageUrl: 'assets/biking.jpeg'
+
+    }
+
+  ];
 }
